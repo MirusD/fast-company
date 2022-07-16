07057c21ac32459f7500bd8272cbb15560edbda3
Damir Salikhov <damir.salihov@inbox.ru>
2022-07-16 18:09:10 +0500

    transferring the state to the app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
-import Users from "./components/users";
+import React, {useState} from 'react'
+import api from "./api"
+import Users from "./components/users"
 
 const App = () => {
-    return <Users/>
+    const [users, setUsers] = useState(api.users.fetchAll())
+
+    const handleDelete = (userId) => {
+        setUsers(prevState => prevState.filter(user => user._id !== userId))
+    }
+    const handleChangeBookmark = (id) => {
+        setUsers(prevState => prevState.map(({ bookmark, ...state}) =>
+            ({...state, bookmark: state._id === id ? !bookmark : bookmark})))
+    }
+
+    return <Users
+        onDeleterUser={handleDelete}
+        onChangeBookmark={handleChangeBookmark}
+        users={users}
+    />
 }
 
 export default App
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -3,9 +3,6 @@ import Qualitie from "./qualitie";
 import Bookmark from "./bookmark";
 
 const User = ({_id: id, name, qualities, profession, completedMeetings, rate, bookmark, onDeleteUser, onChangeBookmark}) => {
-    const handleClickBookmark = () => {
-        onChangeBookmark(id)
-    }
     return (
         <tr>
             <td>{name}</td>
@@ -13,7 +10,7 @@ const User = ({_id: id, name, qualities, profession, completedMeetings, rate, bo
             <td>{profession.name}</td>
             <td>{completedMeetings}</td>
             <td>{rate}/5</td>
-            <td><Bookmark bookmark={bookmark} onClick={handleClickBookmark}/></td>
+            <td><Bookmark bookmark={bookmark} onClick={() => onChangeBookmark(id)}/></td>
             <td><button onClick={() => onDeleteUser(id)} className="btn btn-danger">Удалить</button></td>
         </tr>
     )
