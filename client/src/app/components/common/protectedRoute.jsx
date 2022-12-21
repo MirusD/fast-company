52f711410dfa5366ad2c885c58b72e8ea2b8255b
Damir Salikhov <damir.salihov@inbox.ru>
2022-12-21 20:14:36 +0500

    fix remove console.log

--- client/src/app/components/common/protectedRoute.jsx
+++ client/src/app/components/common/protectedRoute.jsx
@@ -6,7 +6,6 @@ import { getIsLoggedIn } from '../../store/users'
 
 const ProtectedRoute = ({ component: Component, children, ...rest }) => {
     const isLoggedIn = useSelector(getIsLoggedIn())
-    console.log('isLoggenIn:', isLoggedIn)
     return (
         <Route
             {...rest}
